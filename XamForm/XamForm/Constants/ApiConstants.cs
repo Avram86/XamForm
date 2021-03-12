bfa800b49473ef9fcee7a7d06d351e65d39b28f7
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-03-12 12:26:33 +0200

    Added NSwag Dto Model classes and generic repository, IoC

---
+++ XamForm/XamForm/Constants/ApiConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamForm.Constants
+{
+    public class ApiConstants
+    {
+        public const string BaseApiUrl = "http://82.79.49.242:5060/";
+
+        public const string GetArticole = "api/Aplicatie/GetArticole";
+        public const string GetArticoleById="api/Aplicatie/GetArticolByIdArt";
+        public const string GetCategorii = "api/Aplicatie/GetCategorii";
+        public const string GetGrupe = "api/Aplicatie/GetGrupe";
+    }
+}
